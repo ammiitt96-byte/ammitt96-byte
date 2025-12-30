ce2b0bbb7ef08afef715568b5e518722acc4d921
ammiitt96-byte <ammiitt.96@gmail.com>
2025-12-30 13:44:47 +0530

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,5 @@
 const SHEET_URL =
-"https://docs.google.com/spreadsheets/d/1jpR2_9X8QAPErcKe80hnmeI6ztw1ctzh4wE6cQ0njLA/gviz/tq?tqx=out:json";
+"https://docs.google.com/spreadsheets/d/1jpR2_9X8QAPErcKe80hnmeI6ztw1ctzh4wE6cQ0njLA/gviz/tq?tqx=out:json&nocache=" + Date.now();
 
 let chart;
 let allData = [];
@@ -116,3 +116,4 @@ function drawChart(rows) {
     options:{plugins:{legend:{display:false}}}
   });
 }
+
